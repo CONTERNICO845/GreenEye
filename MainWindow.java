62ab2ca64ddd41bc291e9c2b88a299f181879414
Sirio-caba4 <espinozalopezmanuel6@gmail.com>
2026-05-12 19:20:30 -0600

    bluetho

--- MainWindow.java
+++ MainWindow.java
@@ -34,6 +34,6 @@ public class MainWindow {
 
         // Initializes and connects the Bluetooth controller
         ControladorBluetooth bluetooth = new ControladorBluetooth(); // Creates a new instance of ControladorBluetooth
-        bluetooth.conectar("COM4"); // Connects to the Bluetooth device on COM port 3
+        bluetooth.conectar("COM8"); // Connects to the Bluetooth device on COM port 3
     }
 }
